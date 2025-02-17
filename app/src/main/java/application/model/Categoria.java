26c1be2a62075dc5fa8de034b7bc36ef0c9a7a8b
André Espanhol <andrepontes13579@gmail.com>
2025-02-17 17:50:57 +0000

    editando categoria

--- app/src/main/java/application/model/Categoria.java
+++ app/src/main/java/application/model/Categoria.java
@@ -19,5 +19,33 @@ public class Categoria {
     @Id
     @GeneratedValue(Strategy = GenerationType.IDENTITY)
     private long id;
+    @column(unique = true, nullable = false)
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public long getId(){
+        return id;
+    }
     
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public Set<Jogo> getJogos(){
+        return jogos;
+    }
+
+    public void setJogos(Set<Jogo> jogos){
+        this.jogos = jogos;
+    }
 }
